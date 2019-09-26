a23b718e2c222469d9d8615c1e8a299168173838
Matheus de Mello Queiroz <m.dimello@hotmail.com>
2019-09-25 21:21:32 -0300

    Hello, World

---
+++ app/src/main/java/me/mqueiroz/moviepedia/MainActivity.kt
@@ -0,0 +1,18 @@
+package me.mqueiroz.moviepedia
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import me.mqueiroz.home.HomeFragment
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(content.id, HomeFragment())
+            .commit()
+    }
+}
---
+++ home/src/main/java/me/mqueiroz/home/HomeFragment.kt
@@ -0,0 +1,5 @@
+package me.mqueiroz.home
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.fragment_home)
